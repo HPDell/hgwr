b04c7b4a7796fe389f1b873000def95e60f9b98f
HPDell <huyg0180110559@outlook.com>
2022-03-29 20:08:02 +0100

    edit: constructors

--- include/hlmgwr.h
+++ include/hlmgwr.h
@@ -11,6 +11,21 @@ struct HLMGWRArgs {
     arma::mat u;
     arma::uvec group;
     double bw;
+
+    HLMGWRArgs() : G(), X(), Z(), y(), u(), group(), bw(0.0)
+    {
+    }
+
+    HLMGWRArgs(arma::mat in_G, arma::mat in_X, arma::mat in_Z, arma::vec in_y, arma::mat in_u, arma::uvec in_group, double in_bw) :
+        G(in_G),
+        X(in_X),
+        Z(in_Z),
+        y(in_y),
+        u(in_u),
+        group(in_group),
+        bw(in_bw)
+    {
+    }
 };
 
 struct HLMGWRParams {
@@ -21,13 +36,24 @@ struct HLMGWRParams {
 };
 
 struct HLMGWROptions {
-    double alpha = 0.01;
-    double eps_iter = 1e-6;
-    double eps_gradient = 1e-6;
-    size_t max_iters = (size_t)1e6;
-    size_t max_retries = 10;
-    size_t verbose = 0;
-    size_t ml_type = 0;
+    double alpha;
+    double eps_iter;
+    double eps_gradient;
+    size_t max_iters;
+    size_t max_retries;
+    size_t verbose;
+    size_t ml_type;
+
+    HLMGWROptions() 
+    {
+        alpha = 0.01;
+        eps_iter = 1e-6;
+        eps_gradient = 1e-6;
+        max_iters = (size_t)1e6;
+        max_retries = (size_t)10;
+        verbose = (size_t)0;
+        ml_type = (size_t)0;
+    }
 };
 
 HLMGWRParams backfitting_maximum_likelihood(const HLMGWRArgs& args, const HLMGWROptions& options);
