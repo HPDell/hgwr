d0e86a07cb694413ceeef6e0c52a8c81b90a6e95
Yigong Hu <huyg0180110559@outlook.com>
2024-08-15 14:45:36 +0100

    Feature: F test for GLSW effects (#32)
    * add: f test
    
    * fix: function error
    
    * fix: diagB
    
    * fix: c
    
    * Revert "fix: c"
    
    This reverts commit 1edc0195069957a753544f3b0283825cb0e2221f.
    
    * fix: use V
    
    * fix: trace of squared matrix
    
    * edit: simplify large matrices
    
    * edit: temporally remove sparse mat
    
    * edit: avoid multiply V too many times
    
    * edit: combine sparse mat with partial multiply
    
    * fix: use mat array instead of sparse mat
    
    * fix: the way dealing with sigma
    
    * add: f test control fit
    
    * edit: set default value as false for f_test of fit
    
    * fix: compile errors and warnings

--- include/hlmgwr.h
+++ include/hlmgwr.h
@@ -32,14 +32,6 @@ public:  // Type defs
 
     typedef arma::vec (*GWRKernelFunctionSquared)(arma::vec, double);
 
-    enum class BwOptimCriterionType
-    {
-        CV,
-        AIC
-    };
-
-    typedef double (*BwOptimCriterion)(double, void*);
-
     static double actual_bw(arma::vec d, double bw)
     {
         arma::vec ds = sort(d);
@@ -113,7 +105,16 @@ public:  // Type defs
         std::reference_wrapper<arma::rowvec> rVsigma;
         std::reference_wrapper<arma::uvec> group;
         GWRKernelFunctionSquared kernel;
+        PrintFunction printer;
     };
+
+    enum class BwOptimCriterionType
+    {
+        CV,
+        AIC
+    };
+
+    typedef double (*BwOptimCriterion)(double, void*);
 
     static double bw_criterion_cv(double bw, void* params);
 
@@ -303,14 +304,15 @@ public:
 
 public:
     int bw_optimisation(double lower, double upper, const BwSelectionArgs* args);
-    void fit_gwr();
+    void fit_gwr(const bool f_test = false);
     arma::vec fit_gls();
     double fit_D(ML_Params* params);
     double fit_D_beta(ML_Params* params);
     void fit_mu();
     double fit_sigma();
-    Parameters fit();
+    Parameters fit(const bool f_test = false);
     void calc_var_beta();
+    std::vector<arma::vec4> test_glsw();
 
 private:
     /* data */
@@ -358,11 +360,14 @@ private:
     arma::uword nvg;
     arma::uword nvx;
     arma::uword nvz;
+    std::vector<arma::span> group_span;
 
     /* diagnostic information */
     double loglik = 0;
     arma::vec trS;
     arma::vec var_beta;
+    arma::vec trQ;
+    double glsw_sigma = 0.0;
 };
     
 }
